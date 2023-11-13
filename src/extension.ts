9c374894356f0ffccb08339a9ee105ae96b28a99
Stefan Armijo <stefan.armijo@gmail.com>
2023-11-12 23:08:30 -0500

    implement tabs toggling

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,33 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tabs-toggle" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "tabs-toggle" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tabs-toggle.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tabs-toggle!');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('tabs-toggle.toggleTabs', () => {
+    // The code you place here will be executed every time your command is executed
 
-	context.subscriptions.push(disposable);
+    // if editor tabs bar is visible, hide it
+    if (vscode.workspace.getConfiguration().get("workbench.editor.showTabs") === "multiple") {
+      // config.workbench.editor.showTabs: "multiple"
+      // editorTabsVisible: true
+      vscode.workspace.getConfiguration().update("workbench.editor.showTabs", "none", true);
+    }
+    // if editor tabs bar is hidden, show it
+    else {
+      // config.workbench.editor.showTabs: "none"
+      // editorTabsVisible: false
+      vscode.workspace.getConfiguration().update("workbench.editor.showTabs", "multiple", true);
+    }
+
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
